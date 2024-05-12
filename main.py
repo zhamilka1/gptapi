e3a731de9d55802f9a4995c2f81d36fae854f0c1
zhamilka1 <49374608+zhamilka1@users.noreply.github.com>
2024-05-12 18:15:08 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from g4f.client import Client
 from fastapi import FastAPI
 from pydantic import BaseModel
-
+import g4f.Provider.Llama
 
 client = Client()
 
@@ -14,7 +14,8 @@ app = FastAPI()
 async def send_message(message: Message):
 
     response2 = client.chat.completions.create(
-        model="gpt-3.5-turbo",
+        model="Meta-Llama-3-70B-Instruct",
         messages=[{"role": "user", "content": message.text}],
+        provider= g4f.Provider.Llama
     )
     return response2.choices[0].message.content
